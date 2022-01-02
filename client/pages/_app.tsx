d3eb120eb63ba122affdde31262a46930928a6ba
unknown <kainesv86@gmail.com>
2022-01-02 18:34:13 +0700

    Conn - redux store

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,20 +1,27 @@
 import "../styles/globals.css";
 import * as React from "react";
+
 import SideBar from "../components/sideBar";
-import type { AppProps } from "next/app";
 import NavBar from "../components/navbar";
+
+import type { AppProps } from "next/app";
+
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }: AppProps) {
         const [activeBurger, setActiveBurger] = React.useState<boolean>(false);
 
         return (
-                <div className="bg-[#CDC8B0] h-screen flex relative">
-                        <SideBar isActive={activeBurger} setActive={setActiveBurger} />
-                        <div className="flex-1 flex flex-col">
-                                <NavBar isActive={activeBurger} setActive={setActiveBurger} />
-                                <Component {...pageProps} />
+                <Provider store={store}>
+                        <div className="bg-[#CDC8B0] h-screen flex relative">
+                                <SideBar isActive={activeBurger} setActive={setActiveBurger} />
+                                <div className="flex-1 flex flex-col">
+                                        <NavBar isActive={activeBurger} setActive={setActiveBurger} />
+                                        <Component {...pageProps} />
+                                </div>
                         </div>
-                </div>
+                </Provider>
         );
 }
 
